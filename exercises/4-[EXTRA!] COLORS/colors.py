3c6568943a123c1346853203fb3f9baf50e25623
Henriquedev_ <162921668+Henriquedev0506@users.noreply.github.com>
2025-01-31 09:30:13 -0300

    colors1.0

--- exercises/4-[EXTRA!] COLORS/colors.py
+++ exercises/4-[EXTRA!] COLORS/colors.py
@@ -1,1 +1,7 @@
-print('hello world!')
+print('\033[4;30;45mHello world!\033[m')
+
+print('\033[7;30mHello world!\033[m')
+
+print('\033[0;33;44mHello world!\033[m')
+
+print('\033[1;35;43mHello world!\033[m')
