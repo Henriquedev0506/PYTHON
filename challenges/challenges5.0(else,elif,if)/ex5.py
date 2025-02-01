5a6119a5ca6cad9cb21d8e77fe62bf950c638826
Henriquedev_ <162921668+Henriquedev0506@users.noreply.github.com>
2025-02-01 16:26:01 -0300

    Update ex5.py

--- challenges/challenges5.0(else,elif,if)/ex5.py
+++ challenges/challenges5.0(else,elif,if)/ex5.py
@@ -2,10 +2,12 @@ not1 = float(input('primeira nota: '))
 not2 = float(input('segunda nota: '))
 media = (not1 + not2) / 2
 print('Tirando {:.1f} e {:.1f}, a media do aluno é {:.1f}'.format(not1, not2, media))
-if media >= 5and media < 7:
+if 7 > media >=5:
     print('O aluno está em RECUPERAÇAO.')
-elif: 
-
+elif media < 5:
+    print('O aluno foi REPROVADO')
+elif media >=7:
+    print('o aluno esta APROVADO')
 
 
 
