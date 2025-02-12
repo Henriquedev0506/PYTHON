3cfd1400b0b199d9304a13022112e80c3315f576
Henriquedev_ <162921668+Henriquedev0506@users.noreply.github.com>
2025-02-12 11:07:26 -0300

    ex8

--- challenges/challenges6(for)/ex8.py
+++ challenges/challenges6(for)/ex8.py
@@ -1,8 +1,9 @@
 frase = str(input('Digite uma frase: ')).strip().upper()
 palavras = frase.split()
 junto = ''.join(palavras)
-invberso = ''
-for letra in range (len(
-
-
-
+inverso = junto[::-1]
+print('O inverso de {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('Temos um palindromo')
+else:
+    print('A frase digitada nao e um palindromo')
